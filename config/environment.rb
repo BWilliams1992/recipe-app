3be27d0e8d09108f82713a702c781409b04506fb
Ben Williams <bwilliams0001992@gmail.com>
2020-06-14 20:14:08 +0100

    Added sendgrid and dotenv gems. Set up emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+require 'dotenv/load'
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
