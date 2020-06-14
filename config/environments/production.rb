5568d866460fe61776a753713a975fdcb8ae16a5
Ben Williams <bwilliams0001992@gmail.com>
2020-06-14 20:51:58 +0100

    added mailer production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'serene-woodland-88939.herokuapp.com', protocol: "https" }
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
