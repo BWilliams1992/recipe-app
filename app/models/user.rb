69bad6924959afbc7bc3472f96a0c184e33e710e
Ben Williams <bwilliams0001992@gmail.com>
2020-06-21 12:49:35 +0100

    added user to comments, styled comments partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :recipes, dependent: :destroy
   has_many :collections, dependent: :destroy
+  has_many :comments
   has_one_attached :avatar
 end
