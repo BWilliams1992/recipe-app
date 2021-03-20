ae1580c4a17293441dfc6b4dff2cd9d0e0c1f0ca
Ben Williams <bwilliams0001992@gmail.com>
2021-03-20 15:59:49 +0000

    created model spec for user

---
+++ spec/features/recipe_creation_via_form_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "Creates a new recipe from the new recipe from which is saved to the database with the corresponding attributes" do
+  scenario "successfully" do
+    user = create(:user)
+    login_as(user, :scope => :user)
+    
+    visit new_recipe_path
+
+    expect(page).to have_css 'h1', text:'Create Recipe'
+
+    fill_in "Title", with: "Pizza"
+    fill_in "Description", with: "It's a pizza"
+    fill_in "recipe_ingredients_attributes_0_name", with: "Pizza"
+    fill_in "recipe_ingredients_attributes_0_amount", with: 1
+    fill_in "Method", with: "Put in oven and cook it"
+
+    click_on "Create Recipe"
+
+    expect(Recipe.count).to eq(1)
+
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 require_relative 'support/factory_bot.rb'
+require_relative 'support/devise.rb'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
